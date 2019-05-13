2404a891f057716487f94ca9cd84457cac245a79
DeLaRuss <geolo1996@gmail.com>
2019-05-12 23:38:58 -0700

    Add files via upload
    V1.3.3
    #Add placeholders for collider class
    #Creating vector to store bullets

---
+++ CosmicClash-modifying/Game.h
@@ -0,0 +1,53 @@
+#ifndef Game_hpp
+#define Game_hpp
+
+#include "AppComponent.h"
+#include "AnimatedRect.h"
+#include "Timer.h"
+
+class Game: public AppComponent, private Timer{
+	
+	const char* blueshipfile;
+	const char* orangeshipfile;
+	const char* jellyfishfile;
+	const char* spacebgfile;
+
+    TexRect* blueship; //mushroom
+	TexRect* orangeship;
+	TexRect* jellyfish;
+
+	TexRect* bluebullet;
+	TexRect* orangebullet;
+    
+	TexRect* background;
+    TexRect* secondbg;
+    TexRect* thirdbg;
+
+    bool blueshipV;
+    bool orangeshipV;
+	bool jellyfishV;
+	bool bluebulletV;
+	bool orangebulletV;
+    bool firebullet;
+	bool hit;
+	bool forward;
+
+	float inc;
+
+	
+public:
+    AnimatedRect* explosion;
+    Game();
+    
+    void draw() const; // removed const
+    void handleKeyDown(unsigned char, float, float);
+    void action();
+	
+	//friend void timer(int);
+
+    ~Game();
+
+
+};
+
+#endif 
---
+++ CosmicClash-modifying/collider.h
@@ -0,0 +1,17 @@
+//Header
+
+#ifndef collider
+#define collider
+
+#include<iostream>
+#include<string>//might change
+using namespace std;
+
+class bullet {
+public:
+	bullet();
+
+private:
+	//???IDK
+
+#endif
